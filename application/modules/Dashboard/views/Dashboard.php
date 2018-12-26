4a0da8f39e9c84256cfe363cb5f333726fc56808
Andika Pratama <apratama918@gmail.com>
2018-12-26 15:48:05 +0700

    Set Framework and Template

---
+++ application/modules/Dashboard/views/Dashboard.php
@@ -0,0 +1,41 @@
+<body style="background-color: #f1f2f6;">
+    <div class="docs-header">
+      <!--nav-->
+      <nav class="navbar navbar-default navbar-custom" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html"><img src="<?php echo base_url() ?>asset/vendor/bootflat/img/logo.png" height="40"></a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a class="nav-link current" href="getting-started.html">Dashboard</a></li>
+              <li><a class="nav-link" href="documentation.html">Result</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div class="topic">
+        <div class="container">
+			<div class="col-md-8">
+			  <h3>Documentation</h3>
+			  <h4>The complete style of the Bootflat Framework</h4>
+			</div>
+			<div class="col-md-4">
+          </div>
+        </div>
+        <div class="topic__infos">
+          <div class="container">
+            Bootflat offers over a dozen reusable components, which can be combined with each other.
+          </div>
+        </div>
+      </div>
+    </div>
+
+</body>
